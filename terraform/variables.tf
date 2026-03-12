408d91717893d011d878687cfd6f73d09b336f28
de-bunthong <bun.thong@dataengine.co.nz>
2026-03-13 12:21:24 +1300

    t

---
+++ terraform/variables.tf
@@ -0,0 +1,33 @@
+variable "snowflake_organization_name" { type = string }
+variable "snowflake_account_name" { type = string }
+variable "snowflake_user" { type = string }
+variable "snowflake_role" { type = string }
+variable "snowflake_authenticator" { type = string }
+
+variable "edc_db" { type = string }
+
+variable "source_db" { type = string }
+
+variable "warehouse" { type = string }
+variable "target_lag_maximum_duration" { type = string }
+
+variable "access_db" { type = string }
+variable "access_schema" { type = string }
+variable "access_table" { type = string }
+
+variable "target_account" {
+  description = "Account keys to filter deployable tables e.g. [\"apac\", \"na\"]"
+  type        = list(string)
+  default     = ["apac", "na"]
+}
+
+variable "default_account" {
+  description = "Fallback account key for tables without a meta.json"
+  type        = string
+  default     = ""
+}
+
+variable "snowflake_private_key" {
+  type      = string
+  sensitive = true
+}
